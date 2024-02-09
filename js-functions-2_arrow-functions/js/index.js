0f2daf858b95746a391f23b942eb387a273e0528
Krischan <krischanklug@@icloud.com>
2024-02-09 15:37:38 +0100

    finished lesson

--- js-functions-2_arrow-functions/js/index.js
+++ js-functions-2_arrow-functions/js/index.js
@@ -5,7 +5,7 @@ Rewrite the following functions as arrow functions.
 Use implicit returns when possible
 */
 
-function getCurrentHour() {
+const getCurrentHour = () => {
   const now = new Date();
   const currentHour = now.getHours();
   if (currentHour === 0) {
@@ -17,21 +17,21 @@ function getCurrentHour() {
   } else {
     return currentHour - 12 + "pm";
   }
-}
+};
 
-function getVectorLength(x, y, z) {
+const getVectorLength = (x, y, z) => {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+};
 
-function cleanInput(string) {
+const cleanInput = (string) => {
   return string.toLowerCase().trim();
-}
+};
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -41,8 +41,12 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
 
-const add3 = (a, b, c) => a + b + c;
+function add3(a, b, c) {
+  a + b + c;
+}
 
-const repeat10 = (string) => string.repeat(10);
+function repeat10(string) {
+  string.repeat(10);
+}
