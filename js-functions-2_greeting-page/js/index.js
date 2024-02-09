1e477b06e6146edbd4f7d76c7dd9cf5492799389
Krischan <krischanklug@@icloud.com>
2024-02-09 15:33:34 +0100

    finished lesson

--- js-functions-2_greeting-page/js/index.js
+++ js-functions-2_greeting-page/js/index.js
@@ -22,11 +22,43 @@ Change the contents of this page depending on the current day and time.
 
 const display = document.querySelector('[data-js="display"]');
 
+currentTime = new Date().getHours();
+currentWeekDay = new Date().getDay();
+console.log(currentWeekDay);
+
 function getGreeting() {
+  if (new Date().getHours() > 6 && new Date().getHours() < 12) {
+    return "Good Morning";
+  }
+  if (new Date().getHours() > 13 && new Date().getHours() < 18) {
+    return "Good Afternoon";
+  }
+  if (new Date().getHours() > 19 && new Date().getHours() < 22) {
+    return "Good Evening";
+  }
+  if (new Date().getHours() > 23 && new Date().getHours() < 5) {
+    return "Good Night";
+  }
+
   // Code here
 }
 
 function getDayColor() {
+  if (currentWeekDay === 1) {
+    return "darkgrey";
+  }
+  if (
+    currentWeekDay === 2 ||
+    currentWeekDay === 3 ||
+    currentWeekDay === 4 ||
+    currentWeekDay === 5
+  ) {
+    return "lightblue";
+  }
+  if (currentWeekDay === 6 || currentWeekDay === 7) {
+    return hotpink;
+  }
+
   // Code here
 }
 
