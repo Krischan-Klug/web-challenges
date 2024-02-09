63c1a3d060c6dec9a45403f2ab0e2baa2798bc2a
Krischan <krischanklug@@icloud.com>
2024-02-09 15:04:26 +0100

    unfinished lesson

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -6,18 +6,34 @@ const pizzaInput2 = document.querySelector('[data-js="pizza-input-2"]');
 const pizza2 = document.querySelector('[data-js="pizza-2"]');
 const outputSection = document.querySelector('[data-js="output-section"]');
 
-const output = document.querySelector('[data-js="output"]');
+let output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
+  diameter1 = pizzaInput1.value;
+  diameter2 = pizzaInput2.value;
+  calculatePizzaGain(diameter1, diameter2);
+
   // write your code here
 });
 
 pizzaInput2.addEventListener("input", () => {
+  diameter1 = pizzaInput1.value;
+  diameter2 = pizzaInput2.value;
+  calculatePizzaGain(diameter1, diameter2);
+
   // write your code here
 });
 
 // Task 1
 // define the function calculatePizzaGain here
+
+function calculatePizzaGain(diameter1, diameter2) {
+  const pizzaArea1 = (diameter1 / 2) * (diameter1 / 2) * Math.PI;
+  const pizzaArea2 = (diameter2 / 2) * (diameter2 / 2) * Math.PI;
+  const pizzaGain = ((pizzaArea2 - pizzaArea1) / pizzaArea1) * 100;
+  let textContent = Math.round(pizzaGain);
+  output = textContent;
+}
 
 // Task 2
 // define the function updatePizzaDisplay here
