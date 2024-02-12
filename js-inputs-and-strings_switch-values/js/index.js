0f0e27a3a93cceb2615fad729e2caf9cef0587b3
Krischan <krischanklug@@icloud.com>
2024-02-12 15:40:40 +0100

    finished lesson

--- js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings_switch-values/js/index.js
@@ -16,6 +16,7 @@ Hint: the HTML entity for the switch button is: &updownarrow
 const firstInput = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
 
 uppercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toUpperCase();
@@ -23,4 +24,15 @@ uppercaseButton.addEventListener("click", () => {
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  if (firstInput.value === firstInput.value.toLowerCase()) {
+    firstInput.value = firstInput.value.toUpperCase();
+    return;
+  }
+
+  if (firstInput.value === firstInput.value.toUpperCase()) {
+    firstInput.value = firstInput.value.toLowerCase();
+  }
 });
