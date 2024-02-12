a27f62702935ec018bb3bed2bf96d9853f9a5b0e
Krischan <krischanklug@@icloud.com>
2024-02-12 16:35:32 +0100

    finished lesson

--- js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-and-strings_multiple-inputs/js/index.js
@@ -14,3 +14,35 @@ Implement the following functionality:
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
 const button = document.querySelector("[data-js=button]");
+
+let jobIndex = 0;
+
+button.addEventListener("click", (event) => {
+  if (jobIndex === 0) {
+    secondInput.value = firstInput.value;
+    jobIndex = jobIndex + 1;
+    console.log(jobIndex);
+    return;
+  }
+  if (jobIndex === 1) {
+    secondInput.value = firstInput.value.toUpperCase();
+    jobIndex = jobIndex + 1;
+    console.log(jobIndex);
+    return;
+  }
+  if (jobIndex === 2) {
+    let tempFirst = firstInput.value;
+    let tempSecond = secondInput.value;
+    firstInput.value = tempSecond;
+    secondInput.value = tempFirst;
+    jobIndex = jobIndex + 1;
+    console.log(jobIndex);
+    return;
+  }
+  if (jobIndex === 3) {
+    firstInput.value = null;
+    secondInput.value = null;
+    jobIndex = 0;
+    console.log(jobIndex);
+  }
+});
