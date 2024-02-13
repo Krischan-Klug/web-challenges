78aa639c58d00ad9a89faae2d2fc1894302be7b9
Krischan Klug <krischanklug@icloud.com>
2024-02-13 20:25:48 +0100

    finished lesson

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -3,6 +3,17 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
+
+function showSuccessMessage() {
+  successMessage.removeAttribute("hidden");
+}
+
+function hideSuccessMessage() {
+  successMessage.setAttribute("hidden", "");
+}
+
+hideSuccessMessage();
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,40 +27,39 @@ tosCheckbox.addEventListener("input", (event) => {
   let checkBox = event.target.checked;
   console.log(checkBox);
 
+  /*
   if (checkBox) {
     hideTosError();
   } else {
     showTosError();
   }
-  /*
-  
+  */
+
   if (!checkBox) {
     showTosError();
   }
   if (checkBox) {
-    hideTosError;
+    hideTosError();
   }
-
-  */
 });
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let formElements = event.target.elements;
+  const formElements = event.target.elements;
   let tosState = formElements.tos.checked;
 
   // --v-- write your code here --v--
-  const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData);
 
   console.log(tosState);
 
   if (tosState === false) {
     return;
   }
+  showSuccessMessage();
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
+
   alert("Form submitted");
 });
