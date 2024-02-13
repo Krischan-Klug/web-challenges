411d9442c49fe335bb7533994b2f26300f2a4231
Krischan Klug <krischanklug@icloud.com>
2024-02-14 00:13:18 +0100

    finished lesson

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -21,12 +21,50 @@ function divide(a, b) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  /*
+  //get states from inputs
+  const additionInputState = document.querySelector(
+    '[data-js="addition"]'
+  ).checked;
 
+  const subtractionInputState = document.querySelector(
+    '[data-js="subtraction"]'
+  ).checked;
+
+  const multiplicationInputState = document.querySelector(
+    '[data-js="multiplication"]'
+  ).checked;
+
+  const divisionInputState = document.querySelector(
+    '[data-js="addition"]'
+  ).checked;
+*/
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  let numberA = data.numberA;
+  let numberB = data.numberB;
   let result;
 
-  // --v-- write your code here --v--
+  //Debug
+  console.log(data);
+  console.log(data.operator);
+  console.log(numberA);
+  console.log(numberB);
 
-  // --^-- write your code here --^--
+  if (data.operator === "addition") {
+    result = add(numberA, numberB);
+  }
+  if (data.operator === "subtraction") {
+    result = subtract(numberA, numberB);
+  }
+
+  if (data.operator === "multiplication") {
+    result = multiply(numberA, numberB);
+  }
+  if (data.operator === "division") {
+    result = divide(numberA, numberB);
+  }
 
   resultOutput.textContent = result;
 });
