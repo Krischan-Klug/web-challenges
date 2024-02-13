226c58d5e90e98d467957afe81b2fbf08816dfee
Krischan <krischanklug@@icloud.com>
2024-02-13 16:02:10 +0100

    finished lesson

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,24 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  const calcAgeBadness =
+    event.target.elements.age.value + event.target.elements.badness.value;
+
+  console.log(
+    "The age-badness-sum of " +
+      event.target.elements.firstName +
+      " is " +
+      calcAgeBadness
+  );
+
+  console.log(data);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
