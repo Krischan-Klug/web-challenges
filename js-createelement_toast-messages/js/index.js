d58958b94c2b94ddbab4fd9c2a2023a5bd6d2016
Krischan <krischanklug@@icloud.com>
2024-02-14 11:28:25 +0100

    finished lesson

--- js-createelement_toast-messages/js/index.js
+++ js-createelement_toast-messages/js/index.js
@@ -4,10 +4,23 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
+function createMessage(message) {
+  const newLiItem = document.createElement("li");
+  newLiItem.textContent = message;
+
+  newLiItem.classList.add("toast-container__message");
+
+  toastContainer.appendChild(newLiItem);
+
+  console.log(newLiItem.textContent);
+}
+
 addButton.addEventListener("click", () => {
+  createMessage("test");
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  toastContainer.innerHTML = "";
   // Exercise: Clear the stack of toast messages
 });
