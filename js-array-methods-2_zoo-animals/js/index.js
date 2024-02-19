3d95ae49e926e67ece5aa1eeb9dce7fcd62f943d
Krischan <krischanklug@@icloud.com>
2024-02-19 11:41:43 +0100

    finished lesson

--- js-array-methods-2_zoo-animals/js/index.js
+++ js-array-methods-2_zoo-animals/js/index.js
@@ -5,7 +5,9 @@ import { zooAnimals } from "../utils/db.js";
 // The function should return 'true' if the animal name is included in the array or 'false' if not.
 
 function hasAnimal(animals, animalName) {
-  return null;
+  const hasAnimal = animals.includes(animalName);
+
+  return hasAnimal;
 }
 
 // Bonus:
