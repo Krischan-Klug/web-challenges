fce40bac7c487a678e686e822f98a7ad2a27e992
Krischan <krischanklug@@icloud.com>
2024-02-19 16:03:57 +0100

    finished lesson

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,18 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const json = await response.json();
 
-fetchData();
+  return json;
+}
+
+await fetchData();
+
+const data = await fetchData();
+const dataResults = data.results;
+
+console.log(dataResults);
+
+console.log(dataResults[2].eye_color);
