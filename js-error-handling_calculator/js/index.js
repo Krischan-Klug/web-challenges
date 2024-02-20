fb96df13160de2d582f0a6c7cc5b3746aaee6bf5
Krischan <krischanklug@@icloud.com>
2024-02-20 11:19:40 +0100

    finished lesson

--- js-error-handling_calculator/js/index.js
+++ js-error-handling_calculator/js/index.js
@@ -2,16 +2,24 @@ console.clear();
 
 const form = document.querySelector("form");
 const output = form.querySelector("output");
+const errorOutput = document.querySelector(".error");
+
+console.log(errorOutput);
 
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
   multiply: (a, b) => a * b,
   divide: (a, b) => {
-    if (b === 0) {
-      throw new Error("Cannot divide by zero!");
+    try {
+      if (b === 0) {
+        throw new Error("Cannot divide by zero!!!!!!!!!");
+      }
+      return a / b;
+    } catch (error) {
+      console.log(error);
+      errorOutput.innerText = error;
     }
-    return a / b;
   },
 };
 
