481fc986e5bc83bd1796cf3ab2c3fac9d05f5f17
Krischan <krischanklug@@icloud.com>
2024-02-20 11:52:57 +0100

    finished lesson

--- js-error-handling_fetch/js/index.js
+++ js-error-handling_fetch/js/index.js
@@ -1,19 +1,36 @@
 console.clear();
 
 const userElement = document.querySelector(".user");
+const errorMessage = document.querySelector(".error");
 
 async function getUser(url) {
   const response = await fetch(url);
-  const json = await response.json();
-  return json.data;
+
+  if (!response.ok) {
+    console.log("Network response: ", response.ok);
+    return null;
+  }
+
+  try {
+    const json = await response.json();
+    return json.data;
+  } catch (error) {
+    console.log("NO JSON!!!", error);
+    errorMessage.textContent = error;
+    return null;
+  }
 }
 
 document.querySelectorAll("button[data-url]").forEach((button) =>
   button.addEventListener("click", async (event) => {
     const user = await getUser(event.target.dataset.url);
-    userElement.innerHTML = `
-    <h2>${user.first_name} ${user.last_name}</h2>
-    <img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
-    `;
+    try {
+      userElement.innerHTML = `
+<h2>${user.first_name} ${user.last_name}</h2>
+<img alt="${user.first_name} ${user.last_name}" src="${user.avatar}"/>
+`;
+    } catch (error) {
+      errorMessage.textContent = error;
+    }
   })
 );
