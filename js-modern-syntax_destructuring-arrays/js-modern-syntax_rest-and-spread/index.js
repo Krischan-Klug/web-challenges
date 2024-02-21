047e5e6f7d3de5f51444ff71db3391ee2ca8b487
Krischan <krischanklug@@icloud.com>
2024-02-21 12:57:38 +0100

    finished lesson

--- js-modern-syntax_destructuring-arrays/js-modern-syntax_rest-and-spread/index.js
+++ js-modern-syntax_destructuring-arrays/js-modern-syntax_rest-and-spread/index.js
@@ -13,6 +13,8 @@ const person = {
   phone: "12345",
 };
 
+export const { age, ...contact } = person;
+
 // example: export const {value 1} = myObject;
 
 // EXERCISE 2
@@ -20,6 +22,7 @@ const person = {
 // all other items should go into a variable called "restOfList".
 
 const list = ["one", "two", "three", "four", "five"];
+export const [first, second, ...restOfList] = list;
 
 // EXERCISE 3
 // a) The `add` function does not work because it needs an array as argument.
@@ -27,11 +30,12 @@ const list = ["one", "two", "three", "four", "five"];
 // c) Make sure you understand what the rest syntax does here.
 // d) Note that the optional chaining `?.` is used to prevent an error to happen.
 
-function add(allNumbers) {
+function add(...allNumbers) {
   return allNumbers.reduce?.((a, b) => a + b);
 }
 
 export const sum = add(3, 2, 3, 2, 1, 2, 3, 4);
+console.log(sum);
 
 // EXERCISE 4
 // a) Below, the `add` function is called with an array as argument.
@@ -39,7 +43,7 @@ export const sum = add(3, 2, 3, 2, 1, 2, 3, 4);
 // c) Make sure you understand what the spread syntax does here.
 
 const numbers = [3, 2, 1];
-export const result = add(numbers);
+export const result = add(...numbers);
 
 // EXERCISE 5
 // a) Create a variable `allFruits` which contains `anotherFruit` and `fruits`
@@ -49,3 +53,6 @@ export const result = add(numbers);
 
 const fruits = ["apple", "banana", "orange", "papaya"];
 const anotherFruit = "cherry";
+
+export const allFruits = [...fruits, anotherFruit];
+console.log(allFruits);
