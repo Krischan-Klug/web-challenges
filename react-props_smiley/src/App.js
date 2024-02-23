4cfc9851447eac48a92b540c8e80ece4a75790bc
Krischan <krischanklug@@icloud.com>
2024-02-23 15:37:02 +0100

    finished lesson

--- react-props_smiley/src/App.js
+++ react-props_smiley/src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Smiley isHappy={true} />
+    </>
+  );
+}
+
+function Smiley({ isHappy }) {
+  return <h1>{isHappy ? "😀" : "😢"}</h1>;
 }
