22b887c63dcd0fe4e483c5ef7e74a91750233bba
Krischan <krischanklug@@icloud.com>
2024-02-23 15:05:28 +0100

    init files

--- react-props_greeting/src/index.js
+++ react-props_greeting/src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
+const rootElement = document.querySelector("#root");
 const root = createRoot(rootElement);
 
 root.render(
