8bc51dfd371899a3fb9cfed0eaf1e0675856c0b6
Krischan <krischanklug@@icloud.com>
2024-02-23 15:27:16 +0100

    finished lesson

--- react-props_sum/src/App.js
+++ react-props_sum/src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Sum valueA={1} valueB={2} />
+    </>
+  );
+}
+
+function Sum({ valueA, valueB }) {
+  return valueA + valueB;
 }
