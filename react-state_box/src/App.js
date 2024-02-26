2b4e261bcfb3bd33b038d35788ba2df2d7fc9423
Krischan <krischanklug@@icloud.com>
2024-02-26 15:40:48 +0100

    finished lesson

--- react-state_box/src/App.js
+++ react-state_box/src/App.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let isActive = false;
+  const [isActive, setIsActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    setIsActive(!isActive);
+
     // Check that the value changes correctly.
     console.log(isActive);
   }
