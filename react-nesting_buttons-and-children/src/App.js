9091de113b919e8e3b0bbc6f4fe311d5f05de8f4
Krischan <krischanklug@@icloud.com>
2024-02-26 11:14:55 +0100

    finished lesson

--- react-nesting_buttons-and-children/src/App.js
+++ react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Hi</Button>
+      <Button>Bye</Button>
+      <Button>Go</Button>
+      <Button>Stop</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
