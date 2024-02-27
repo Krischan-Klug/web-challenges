d6dbdc72ede938193d035eb74c2ef015ef13f6db
Krischan <krischanklug@@icloud.com>
2024-02-27 12:43:23 +0100

    finished lesson

--- react-with-arrays_users/src/components/Tag.js
+++ react-with-arrays_users/src/components/Tag.js
@@ -1,5 +1,7 @@
 import "./Tag.css";
 
 export default function Tag({ tag }) {
-  return <li className="tag">{tag}</li>;
+  const isAdmin = tag === "admin";
+
+  return <li className={`tag ${isAdmin ? "tag--highlight" : ""}`}>{tag}</li>;
 }
