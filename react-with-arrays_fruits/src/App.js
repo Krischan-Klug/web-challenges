fbb4f786931283b495e1ef774de983620fcf560a
Krischan <krischanklug@@icloud.com>
2024-02-27 11:27:27 +0100

    finished lesson

--- react-with-arrays_fruits/src/App.js
+++ react-with-arrays_fruits/src/App.js
@@ -1,11 +1,43 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1338,
+      name: "🍌 Apple",
+      color: "Green",
+    },
+    {
+      id: 1339,
+      name: "🍌 Dog",
+      color: "White",
+    },
+    {
+      id: 1340,
+      name: "🍌 Cat",
+      color: "Black",
+    },
+    {
+      id: 1341,
+      name: "🍌 Cherry",
+      color: "Red",
+    },
+  ];
 
   return (
-    <div className="app">
-      <Card name="🍌 banana" />
-    </div>
+    <ul>
+      {fruits.map((fruit) => {
+        return (
+          <div className="app">
+            <Card color={fruit.color} name={fruit.name} />
+          </div>
+        );
+      })}
+    </ul>
   );
 }
--- react-with-arrays_fruits/src/components/Card.js
+++ react-with-arrays_fruits/src/components/Card.js
@@ -1,5 +1,9 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ name, color }) {
+  return (
+    <p style={{ backgroundColor: color }} className="card">
+      {name}
+    </p>
+  );
 }
