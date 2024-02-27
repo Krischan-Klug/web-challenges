d31e5f9fb16e874eecfbc8eae1a007348e233dac
Krischan Klug <krischanklug@icloud.com>
2024-02-28 00:20:59 +0100

    finished lesson

--- react-state-2_favourite-holiday/src/App.js
+++ react-state-2_favourite-holiday/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
   function handleSubmit(event) {
     event.preventDefault();
+    event.target.reset();
+    event.target.elements.holiday.focus();
   }
+
+  const [holiday, setHoliday] = useState("");
+  const [date, setDate] = useState("");
 
   return (
     <div className="container">
@@ -17,23 +23,29 @@ export default function App() {
       >
         <label htmlFor="holiday">My Favourite Holiday: </label>
         <input
+          onChange={(event) => setHoliday(event.target.value)}
           id="holiday"
           type="text"
           name="holiday"
           placeholder="e.g. Christmas"
         />
         <label htmlFor="date">Date: </label>
-        <input id="date" type="date" name="date" />
+        <input
+          onChange={(event) => setDate(event.target.value)}
+          id="date"
+          type="date"
+          name="date"
+        />
         <button type="submit" className="form__submit-button">
           Submit
         </button>
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );
