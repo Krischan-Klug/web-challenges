334bf1762fd1cdca590509178bf996ed56d1e575
Krischan <krischanklug@@icloud.com>
2024-02-27 15:38:59 +0100

    unfinished lesson

--- react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.js
+++ react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.js
@@ -5,6 +5,36 @@ import Tabs from "../Tabs";
 import Tab from "../Tab";
 import Badge from "../Badge";
 
+const entries = [
+  {
+    id: 1000,
+    date: "Feb 5, 2025",
+    motto: "We are in a state of chaos",
+    notes:
+      "Today I learned about React State. It was fun! I can't wait to learn more.",
+  },
+  {
+    id: 999,
+    date: "Feb 4, 2025",
+    motto: "Props, Props, Props",
+    notes:
+      "Today I learned about React Props. Mad props to everyone who understands this!",
+  },
+  {
+    id: 998,
+    date: "Feb 3, 2025",
+    motto: "How to nest components online fast",
+    notes:
+      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+  },
+  {
+    id: 997,
+    date: "Feb 2, 2025",
+    motto: "I'm a React Developer",
+    notes: "My React-ion when I learned about React: 😍",
+  },
+];
+
 export default function EntriesSection() {
   return (
     <section className="entries-section">
@@ -23,6 +53,7 @@ export default function EntriesSection() {
           notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
         />
         <Divider />
+        {/* 
         <Entry
           date="Feb 26, 2028"
           motto="Thats life in the city"
@@ -33,7 +64,7 @@ export default function EntriesSection() {
           date="Feb 25, 2028"
           motto="Thats life in the city"
           notes="Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si mihi. Et quidem se repellere, idque instituit docere sic omne animal, simul atque."
-        />
+        /> */}
       </div>
     </section>
   );
