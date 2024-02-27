473240d634ace52f75bde5c75437eae7c89104d5
Krischan <krischanklug@@icloud.com>
2024-02-27 16:52:14 +0100

    unfinished lesson

--- react-state-2_table-reservation/src/components/Counter.js
+++ react-state-2_table-reservation/src/components/Counter.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export default function Counter() {
-  const [people, setPeople] = useState(0);
   return (
     <>
       <h2>How many people would you like to visit us with?</h2>
@@ -10,7 +9,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="increment people count"
-          onClick={() => setPeople(people + 1)}
+          onClick={handleIncrement()}
         >
           +
         </button>
@@ -18,7 +17,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement people count"
-          onClick={() => setPeople(people - 1)}
+          onClick={handleDecrement()}
         >
           -
         </button>
