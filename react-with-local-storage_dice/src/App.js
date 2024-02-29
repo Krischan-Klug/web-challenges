24ab0d154ca534b30d4bb27bb01734c360f1c76e
Krischan-Klug <krischanklug@icloud.com>
2024-02-29 11:23:14 +0100

    finished lesson

--- react-with-local-storage_dice/src/App.js
+++ react-with-local-storage_dice/src/App.js
@@ -1,15 +1,26 @@
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
 import "./styles.css";
+import { useState } from "react";
+import { getD6Roll } from "./utils";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+    console.log(rolls);
+  };
+
+  const currentRollValue = rolls[0]?.value;
   return (
     <div className="app">
       <main className="app__main">
-        <D6Button />
+        <D6Button currentRollValue={currentRollValue} onRoll={handleRoll} />
       </main>
       <aside className="app__aside">
-        <History rolls={[]} />
+        <History rolls={rolls} />
       </aside>
     </div>
   );
--- react-with-local-storage_dice/src/components/D6Button/index.js
+++ react-with-local-storage_dice/src/components/D6Button/index.js
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import { getD6Roll } from "../../utils";
 import D6 from "../D6";
 import "./D6Button.css";
 
-export function D6Button() {
-  const [rolls, setRolls] = useState([]);
-
-  const handleRoll = () => {
-    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
-  };
-
-  const currentRollValue = rolls[0]?.value;
-
+export function D6Button({ onRoll, currentRollValue }) {
   return (
-    <button className="d6-button" type="button" onClick={handleRoll}>
+    <button className="d6-button" type="button" onClick={onRoll}>
       <D6 value={currentRollValue} />
     </button>
   );
