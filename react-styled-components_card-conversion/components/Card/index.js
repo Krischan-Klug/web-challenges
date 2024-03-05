76e709114dd100dbf4a08805fa8d7705b9267b9f
Krischan-Klug <krischanklug@icloud.com>
2024-03-05 16:11:44 +0100

    finished lesson

--- react-styled-components_card-conversion/components/Card/index.js
+++ react-styled-components_card-conversion/components/Card/index.js
@@ -1,13 +1,33 @@
+import styled from "styled-components";
+
+const CardWrapper = styled.div`
+  border-radius: 4px;
+  padding: 8px;
+  box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+const CardTitle = styled.h3`
+  margin-top: 0;
+  margin-bottom: 12px;
+  font-size: 1.25rem;
+`;
+
+const CardDescription = styled.p`
+  margin-top: 0;
+  margin-bottom: 4px;
+`;
+
 export default function Card() {
   return (
-    <div className="card">
-      <h3 className="card__title">Homer Simpson</h3>
-      <p className="card__description">
+    <CardWrapper>
+      <CardTitle>Homer Simpson</CardTitle>
+      <CardDescription>
         Homer Jay Simpson is a fictional character and the main protagonist of
         the American animated sitcom The Simpsons. He is voiced by Dan
         Castellaneta and first appeared, along with the rest of his family, in
         The Tracey Ullman Show short Good Night on April 19, 1987.
-      </p>
-    </div>
+      </CardDescription>
+    </CardWrapper>
   );
 }
--- react-styled-components_card-conversion/pages/_app.js
+++ react-styled-components_card-conversion/pages/_app.js
@@ -1,5 +1,4 @@
 import GlobalStyle from "../styles";
-import "../components/Card/Card.css";
 
 export default function App({ Component, pageProps }) {
   return (
