8e6e4a93fad1895830defabf074d729ff10a54a4
Krischan-Klug <krischanklug@icloud.com>
2024-03-07 16:56:25 +0100

    add button variation

--- cities/pages/index.js
+++ cities/pages/index.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+import styled, { css } from "styled-components";
+
 export default function HomePage() {
+  const StyledLink = styled(Link)`
+    color: green;
+
+    &:hover {
+      color: blue;
+    }
+
+    ${(props) => {
+      if (props.variation === "primary") {
+        return css`
+          color: red;
+        `;
+      }
+    }}
+  `;
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <StyledLink variation="primary" href="/cities">
+        <h2>Cities</h2>
+      </StyledLink>
     </div>
   );
 }
