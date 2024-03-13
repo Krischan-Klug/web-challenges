5db35b8069d7ef460f0debe00998981e8303b77f
Krischan-Klug <krischanklug@icloud.com>
2024-03-13 11:58:25 +0100

    finished lesson

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,26 @@
 import { createServer } from "node:http";
+import { Chance } from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+function generateRandomPerson() {
+  return {
+    name: chance.name(),
+    age: chance.age(),
+    profession: chance.profession(),
+  };
+}
+
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+
+    const randomPerson = generateRandomPerson();
+    response.end(
+      `Hello, my name is ${randomPerson.name} and I am ${randomPerson.age} years old. I am a ${randomPerson.profession}.`
+    );
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
+  }
+});
