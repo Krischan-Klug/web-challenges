2beddb870cb3c3495d7c0eb27c43ffcd282da1b8
Krischan-Klug <krischanklug@icloud.com>
2024-03-13 11:01:55 +0100

    finished lesson

--- backend-basics_routes/index.js
+++ backend-basics_routes/index.js
@@ -1,3 +1,7 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+const port = 8000;
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
--- backend-basics_routes/server.js
+++ backend-basics_routes/server.js
@@ -1,3 +1,12 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer((request, response) => {
+  if (request.url === "/api/fish/1") {
+    response.end("Shrimp");
+  } else if (request.url === "/api/fish/2") {
+    response.end("Anemonefish");
+  } else {
+    response.statusCode = 404;
+    response.end("404 - Not found");
+  }
+});
