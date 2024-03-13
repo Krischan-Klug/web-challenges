88ff3c39454181449322129a3c9c8f50fa270de1
Krischan-Klug <krischanklug@icloud.com>
2024-03-13 10:56:37 +0100

    finished lesson

--- backend-basics_greeting/server.js
+++ backend-basics_greeting/server.js
@@ -1,3 +1,10 @@
 import { createServer } from "node:http";
 
+const name = "Krischan";
+
 // export const server = …
+export const server = createServer((request, response) => {
+  console.log(request.method, request.url);
+  response.statusCode = 200;
+  response.end(`Hello, ${name}`);
+});
