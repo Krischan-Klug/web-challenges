d85504b58d81f00c6fa6e620b296b4f557376fa7
Krischan-Klug <krischanklug@icloud.com>
2024-03-15 12:59:34 +0100

    finished lesson

--- backend-read_products/components/ProductList/index.js
+++ backend-read_products/components/ProductList/index.js
@@ -13,13 +13,15 @@ export default function ProductList() {
     return;
   }
 
+  console.log(data);
+
   return (
     <>
       <StyledHeading>Available Fishes</StyledHeading>
       <StyledList>
         {data.map((product) => (
-          <li key={product.id}>
-            <StyledLink href={`/${product.id}`}>{product.name}</StyledLink>
+          <li key={product._id}>
+            <StyledLink href={`/${product._id}`}>{product.name}</StyledLink>
           </li>
         ))}
       </StyledList>
--- backend-read_products/pages/api/products/index.js
+++ backend-read_products/pages/api/products/index.js
@@ -1,5 +1,13 @@
-import { products } from "../../../lib/products";
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
+export default async function handler(request, response) {
+  await dbConnect();
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+  if (request.method === "GET") {
+    const products = await Product.find();
+
+    return response.status(200).json(products);
+  } else {
+    return response.status(405).json({ status: "Method Not Allowed" });
+  }
 }
