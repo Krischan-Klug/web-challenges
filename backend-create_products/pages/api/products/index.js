a60cf705a3b59e285fcb12dbd644262371bc1e61
Krischan-Klug <krischanklug@icloud.com>
2024-03-15 16:21:05 +0100

    finished lesson

--- backend-create_products/pages/api/products/index.js
+++ backend-create_products/pages/api/products/index.js
@@ -8,4 +8,9 @@ export default async function handler(request, response) {
     const products = await Product.find();
     return response.status(200).json(products);
   }
+
+  if (request.method === "POST") {
+    const product = await Product.create(request.body);
+    return response.status(201).json({ message: "Product created" });
+  }
 }
